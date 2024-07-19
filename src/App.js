26c6c7a31bf56c6c13a97115e8ff8d703942c214
Tonny Piper <pipertonny@gmail.com>
2024-07-19 07:50:53 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { HomePage } from "./pages/Home";
+import { ProductPage } from "./pages/ProductsPage";
+import { RootLayout } from "./pages/RootLayout";
+import { Error } from "./pages/Error";
+import { ProductDetailPage } from "./pages/ProductDetailPage";
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <HomePage />
+    element: <RootLayout />,
+    errorElement: <Error />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/products', element: <ProductPage /> },
+      { path: "/products/:productId", element: <ProductDetailPage /> }
+    ]
   }
+
 ])
 function App() {
   return <RouterProvider router={router} />
